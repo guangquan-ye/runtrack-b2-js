2c02cc0b506b35dfb4e103ddafcd61f1ddc5b520
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-09-06 17:36:18 +0200

    add job-03

---
+++ jour-01/job-03/script.js
@@ -0,0 +1,21 @@
+function myIsInString(haystack, needle) {
+    let haystackIndex = 0;
+    let needleIndex = 0;
+
+    while (haystack[haystackIndex] !== undefined) {
+        if (haystack[haystackIndex] === needle[needleIndex]) {
+            needleIndex++;
+            if (needle[needleIndex] === undefined) {
+                return true;
+            }
+        } else {
+            needleIndex = 0;
+        }
+        haystackIndex++;
+    }
+    return false;
+}
+
+console.log(myIsInString("Hello World", "llo"));
+
+
